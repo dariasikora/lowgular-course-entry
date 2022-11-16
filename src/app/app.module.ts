383d2656a2ca8357496413b067069fc76aa8a97a
dariasikora <soltysiak.daria@gmail.com>
2022-11-16 23:49:14 +0100

    feat: create project model & CSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { EmployeeListComponentModule } from "./ui/employee-list/employee-list.component-module";
 import {EmployeeFacesComponentModule} from "./ui/employee-faces/employee-faces.component-module";
 import {EmployeeServiceModule} from "./services/employee.service-module";
+import {ProjectListComponentModule} from "./ui/project-list/project-list.component-module";
+import {ProjectServiceModule} from "./services/project.service-module";
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,7 +17,9 @@ import {EmployeeServiceModule} from "./services/employee.service-module";
     HttpClientModule,
     EmployeeListComponentModule,
     EmployeeFacesComponentModule,
-    EmployeeServiceModule
+    EmployeeServiceModule,
+    ProjectServiceModule,
+    ProjectListComponentModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
