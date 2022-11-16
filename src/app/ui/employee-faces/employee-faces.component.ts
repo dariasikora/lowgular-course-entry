7d250f7c40780e378696858b9b5e57b50ee136bd
dariasikora <soltysiak.daria@gmail.com>
2022-11-16 21:25:25 +0100

    feat: html changed & names added

---
+++ src/app/ui/employee-faces/employee-faces.component.ts
@@ -0,0 +1,23 @@
+
+
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { EmployeeModel } from '../../model/employee.model';
+import {HttpClient} from "@angular/common/http";
+
+
+@Component({
+  selector: 'app-employee-faces',
+  templateUrl: './employee-faces.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class EmployeeFacesComponent {
+  constructor(private _client: HttpClient) {
+  }
+  data$: Observable<EmployeeModel[] | null> = this._client.get<EmployeeModel[]>('assets/data/employees.json');
+}
