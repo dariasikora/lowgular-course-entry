4aea26bedd4ba48e9558fb919150800d0968abbc
dariasikora <soltysiak.daria@gmail.com>
2022-11-18 23:36:11 +0100

    feat: / delete

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -14,7 +14,7 @@ export class EmployeeService {
   }
 
   create(employee: CreateEmployeeModel): Observable<void> {
-    return this._httpClient.post('https://dummy.restapiexample.com/api/v1/create/', employee).pipe(map(_=> void 0));
+    return this._httpClient.post('https://dummy.restapiexample.com/api/v1/create', employee).pipe(map(_=> void 0));
   }
 
   delete(id: string): Observable<void> {
