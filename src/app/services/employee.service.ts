37d6255029923ac16e30f4fe556ee463ac2b11ca
dariasikora <soltysiak.daria@gmail.com>
2022-11-19 18:21:41 +0100

    feat: response added

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {map, Observable} from 'rxjs';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { PersonModel } from '../model/person.model';
 import { CreateEmployeeModel } from '../model/create-employee.model';
+import {ApiResponse} from "./api.response";
+import {EmployeeResponse} from "./employee.response";
 
 @Injectable()
 export class EmployeeService {
@@ -10,11 +13,22 @@ export class EmployeeService {
   }
 
   getAll(): Observable<PersonModel[]> {
-    return this._httpClient.get<PersonModel[]>('assets/data/people.json')
+    return this._httpClient.get<ApiResponse<EmployeeResponse[]>>('https://dummy.restapiexample.com/api/v1/employees').pipe(map((response: ApiResponse<EmployeeResponse[]>) => {
+      return response.data.map((employeeResponse: EmployeeResponse) => {
+        return {
+          personalNumber: employeeResponse.id,
+          name: employeeResponse.employee_name,
+          mail: employeeResponse.employee_name + '@lowgular.io',
+          img: employeeResponse.profile_image,
+
+        }
+      })
+      }));
+
   }
 
   create(employee: CreateEmployeeModel): Observable<void> {
-    return this._httpClient.post('https://dummy.restapiexample.com/api/v1/create', employee).pipe(map(_=> void 0));
+    return this._httpClient.post('https://dummy.restapiexample.com/api/v1/create', employee).pipe(map(_ => void 0));
   }
 
   delete(id: string): Observable<void> {
